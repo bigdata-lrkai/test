861bf712e92ffe2264a67d9868acdb4e76b0976c
bigdata-lrkai <69000858+bigdata-lrkai@users.noreply.github.com>
2024-12-09 17:36:56 +0800

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+
+#' this is a function
+#'
+#' @param name name
+#'
+#' @return hello
+#' @export
+#'
+#' @examples
+#' hello("wang meiheng")
+hello <- function(name) {
+  cat("Hello",name,"\n")
+}
